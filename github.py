dc6401333541a95103274ecaf60d599f983cc8fa
fredokr <90770741+fredokr@users.noreply.github.com>
2021-10-19 10:31:18 +0200

    Update github.py

--- github.py
+++ github.py
@@ -20,14 +20,43 @@
 
 
 
+spark-shell --master spark://master:7077
 
+    
 #Télécharger pip et pandas
 !apt install pip 
 !pip install pandas
 
 #Créer fichier dataframe
 import pandas as pd
-data=pd.read_csv('syslog.csv')
+import re
+import datetime
+datetime = datetime.datetime.now().replace(microsecond=0)
 
-spark-shell --master spark://master:7077
 
+
+df_alternative =pd.read_csv(r'C:\Users\axelg\Downloads\youpi\alternatives.csv',header=None,error_bad_lines=False)
+df_dmesg =pd.read_csv(r'C:\Users\axelg\Downloads\youpi\dmesg.csv',header=None,error_bad_lines=False)
+
+
+#Traitement alternative
+
+df_alternative['raw']=df_alternative[0]
+df_alternative['utilisateur']=df_alternative[0].str.extract('^(\S+)\s',expand=False).str.strip()
+df_alternative[0]=df_alternative[0].str.replace('^(\S+)\s','')
+df_alternative['date']=df_alternative[0].str.extract(r'([0-2][0-9][0-2][0-9]-[0-2][0-9]-[0-2][0-9] [0-2][0-9]:[0-5][0-9]:[0-9][0-9])',expand=False).str.strip()
+df_alternative[0]=df_alternative[0].str.replace(r'([0-2][0-9][0-2][0-9]-[0-2][0-9]-[0-2][0-9] [0-2][0-9]:[0-5][0-9]:[0-9][0-9])','')
+df_alternative['process']="no process"
+df_alternative['message_log']=df_alternative[0]
+df_alternative['source']="alternatives"
+df_alternative['date_traitement']=datetime
+
+#Traitement dmesg
+
+df_dmesg["raw"]=df_dmesg[0]
+df_dmesg["utilisateur"]="no utilisateur"
+df_dmesg["date"]="0000 00 00 00:00:00"
+df_dmesg["process"]="no process"
+df_dmesg["message_log"]=df_dmesg[0]
+df_dmesg["source"]="dmesg"
+df_dmesg['date_traitement']=datetime_object
