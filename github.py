95128be2881359dca2552fa5cb6c47b023f022c3
Studiesuser21 <92428584+Studiesuser21@users.noreply.github.com>
2021-10-19 11:11:19 +0200

    Update github.py

--- github.py
+++ github.py
@@ -38,6 +38,15 @@ datetime = datetime.datetime.now().replace(microsecond=0)
 df_alternative =pd.read_csv(r'C:\Users\axelg\Downloads\youpi\alternatives.csv',header=None,error_bad_lines=False)
 df_dmesg =pd.read_csv(r'C:\Users\axelg\Downloads\youpi\dmesg.csv',header=None,error_bad_lines=False)
 
+#Formatage de la date
+def formatagedate(date):
+    MOIS = {"Jan" : "01","Feb" : "02","Mar" : "03","Apr" : "04","May" : "05","Jun" : "06",
+            "Jul" : "07","Aug" : "08","Sep" : "09","Oct" : "10","Nov" : "11","Dec" : "12",}  
+    liste_date = date.split()
+    if len(liste_date)== 3:
+        mois = MOIS.get("%s"%liste_date[0])
+        date = "2021"+"/"+str(mois)+"/"+ liste_date[1]+" "+liste_date[2]
+    return(date)
 
 #Traitement alternative
 
@@ -55,8 +64,16 @@ df_alternative['date_traitement']=datetime
 
 df_dmesg["raw"]=df_dmesg[0]
 df_dmesg["utilisateur"]="no utilisateur"
-df_dmesg["date"]="0000 00 00 00:00:00"
+df_dmesg["date"]="0000/00/00 00:00:00"
 df_dmesg["process"]="no process"
 df_dmesg["message_log"]=df_dmesg[0]
 df_dmesg["source"]="dmesg"
 df_dmesg['date_traitement']=datetime_object
+
+
+
+
+
+
+
+df['date'] = df['date'].apply(formatagedate) # pour formater les dates sur le dataframe final.
